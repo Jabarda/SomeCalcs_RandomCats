3fbe250c3abeafc9b025ed77380756729d1a1309
PogChamp <stalkercxz@yandex.ru>
2017-12-11 23:25:44 +0300

    Revert "one more try"
    This reverts commit b0ea594ec329b357683f4550533a65bdd22ef033.

--- site/index.php
+++ site/index.php
@@ -9,13 +9,32 @@
   <div>
 	Так как у нас сайт про вычисления и котиков, посчитаем число ПИ по формуле PI = 2 * (1 + 1/3 * (1 + 2/5 * (1 + 3/7 * (...))))</br>
 	Введите точность: 
-	<form action="calcPi.php" method="post">
+	<form role="form" method="post">
 	<input type="text" name="hard" /></br>
-	</form>
 	<input type="button" id="RecCount" value="Посчитать!" />
+	</form>
 	<div id=PiAnswer></div>
 	
   </div>
+  <script>
+	$('#RecCount').click(function(){
+		$('#PiAnswer').html('\
+		<?php
+			echo "Im here!";
+			$max = (int)$_POST["hard"];
+			echo $max;
+			echo rand(5, 15);
+			echo "Im here!";
+			function F ($i)
+			{
+				if ($i>$max) return 0;
+				return 1 + $i / (2.0 * $i + 1) * F($i + 1);
+			}
+			echo "Ответ: " ;
+			echo 2*F(1); ?>\
+		');
+	});
+  </script>
   <script src="js/bootstrap.min.js"></script>
  </body>
 </html>
