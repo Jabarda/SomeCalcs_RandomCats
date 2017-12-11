d0b78cfcc52b34d8c4f6ee8afe93a83d3a56a291
PogChamp <stalkercxz@yandex.ru>
2017-12-11 23:39:07 +0300

    some hack

--- site/index.php
+++ site/index.php
@@ -9,9 +9,7 @@
   <div>
 	Так как у нас сайт про вычисления и котиков, посчитаем число ПИ по формуле PI = 2 * (1 + 1/3 * (1 + 2/5 * (1 + 3/7 * (...))))</br>
 	Введите точность: 
-	<form name="form1" method="post" action="'.$_SERVER['PHP_SELF'].'"
 	<input type="text" name="hard" /></br>
-	</form>
 	<input type="button" id="RecCount" value="Посчитать!" />
 	<div id=PiAnswer></div>
 	
@@ -20,18 +18,14 @@
 	$('#RecCount').click(function(){
 		$('#PiAnswer').html('\
 		<?php
-			echo "Im here!";
-			$max = (int)$_POST["hard"];
-			echo $max;
-			echo rand(5, 15);
-			echo "Im here!";
 			function F ($i)
 			{
-				if ($i>$max) return 0;
+				if ($i>100000) return 0;
 				return 1 + $i / (2.0 * $i + 1) * F($i + 1);
 			}
+			$answer=2*F(1);
 			echo "Ответ: " ;
-			echo 2*F(1); ?>\
+			echo '<div hidden=\"true\" id=\"Answer\" value=\"'.$answer.'\"></div>'; ?>\
 		');
 	});
   </script>
